9fc9c2ad01eaabf91361fd23f7448255057d720b
itsChathurangaJ <chathuranga.jayanidu@gmail.com>
2022-11-01 19:03:50 +0530

    initial setup done

---
+++ src/components/categories/categories.component.jsx
@@ -0,0 +1,12 @@
+import './categories.styles.scss';
+import CategoryItem from '../category-item/category-item.component';
+
+export const Categories = ({ categories }) => {
+  return (
+    <div className='categories-container'>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
